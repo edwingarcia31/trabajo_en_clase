f97d9e98c1806a607ec77e47fd72ff60b00695ad
edwin garcia <edwin.garcia@uam.edu.co>
2016-08-08 20:52:07 -0500

    actualizacion

---
+++ Hola+Mundo.py
@@ -0,0 +1,77 @@
+
+# coding: utf-8
+
+# In[8]:
+
+2+4*6/8.0-3
+
+
+# $$f(x)=li(x)-\sum_{p}^{}li(x \rho)-log(2)+\int_x^\infty\frac{dt}{t(t^2-1log)(t)}$$
+
+# In[16]:
+
+int(_15)
+
+
+# In[10]:
+
+5**4
+
+
+# In[13]:
+
+3/2
+
+
+# In[15]:
+
+5.0/2
+
+
+# In[18]:
+
+float(_10)
+
+
+# In[20]:
+
+complex(_16)
+
+
+# In[22]:
+
+max(1, 3, -5)
+
+
+# In[24]:
+
+num = 10 
+print num
+
+
+# In[30]:
+
+x,y,z,a=1,2,3,4
+
+
+# In[33]:
+
+x,y=a,z
+
+
+# In[34]:
+
+x
+
+
+# In[39]:
+
+x=10
+y=8
+
+if x>y:
+    print('el mayor es ',y)
+else:
+    print ('el menor es ',x)
+    
+
